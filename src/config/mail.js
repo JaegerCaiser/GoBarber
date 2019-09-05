3ed6c695e0abfa8819a2c8584ccb9e56cd90e7b8
Matheus Henrique Caiser <matheus.kaiser@gmail.com>
2019-09-04 23:42:15 -0300

    Conexao com mongo, filas em background, envio de email

---
+++ src/config/mail.js
@@ -0,0 +1,12 @@
+export default {
+  host: 'smtp.mailtrap.io',
+  port: '2525',
+  secure: false,
+  auth: {
+    user: '761e33ac697c1b',
+    pass: '1e238fbe013f82',
+  },
+  default: {
+    from: 'Equipe GoBarber <noreply@gobarber.com>',
+  },
+};
